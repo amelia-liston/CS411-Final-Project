fe2ccb592d823940e820cf1bc3974c3a5fb1aa75
amelia-liston <aliston@bu.edu>
2024-12-10 21:09:00 -0500

    Removed global keyword

--- meal_max_example/global_urls.py
+++ meal_max_example/global_urls.py
@@ -1,4 +1,4 @@
-global REDIRECT_URI = 'http://127.0.0.1:5000/callback'
-global SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
-global SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
-global BASE_URL="https://accounts.spotify.com"
+REDIRECT_URI = 'http://127.0.0.1:5000/callback'
+SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
+SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL="https://accounts.spotify.com"
